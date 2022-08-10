3595ad9cf82b0dfe4398f248597915bbb6e9e8db
Icaro Dias <icarodiasdev@gmail.com>
2022-08-10 13:13:08 -0300

    update all src

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // SEND A STATIC PAGE
 
-app.use(express.static(path.join(__dirname+'/public')))
+app.use(express.static(path.join(__dirname+'/public/version2')))
 
 
 app.get('/pdf', async(request,response)=>{
